3a3c2b015d975e55731385d0384267f3011b2606
benni <krug.benjamin.92@gmail.com>
2021-08-06 14:48:09 +0200

    :construction: behandlungsraum Zahnreihe vereinheitlicht

--- UI/dentistui/src/components/body/components/Tagesbehandlungen/Behandlungsraum/Zaehne/ZahnCard.vue
+++ UI/dentistui/src/components/body/components/Tagesbehandlungen/Behandlungsraum/Zaehne/ZahnCard.vue
@@ -1,27 +1,45 @@
 <template>
   <div class="ZahnCard">
-    <schneide-zahn v-if="type === 'SchneideZahn'" :zahn-style="zahnStyle" :wurzeln="wurzeln" :isUnterKiefer="isUnterKiefer" />
-    <eck-zahn v-if="type === 'EckZahn'" :zahn-style="zahnStyle" :wurzeln="wurzeln" :isUnterKiefer="isUnterKiefer" />
-    <vormahl-zahn v-if="type === 'VormahlZahn'" :zahn-style="zahnStyle" :wurzeln="wurzeln" :isUnterKiefer="isUnterKiefer" />
-    <mahl-zahn v-if="type === 'MahlZahn'" :zahn-style="zahnStyle" :wurzeln="wurzeln" :isUnterKiefer="isUnterKiefer" />
-    <weisheits-zahn v-if="type === 'WeisheitsZahn'" :zahn-style="zahnStyle" :wurzeln="wurzeln" :isUnterKiefer="isUnterKiefer" />
+    <div class="ZahnCardAufbau">
+      <wurzel-selector class="wurzel-selector" :wurzeln="wurzeln"/>
+      <div />
+      <div class="box mitte-mitte"/>
+      <div />
+      <div v-if="type !== 'SchneideZahn' && type !== 'EckZahn'" />
+      <div v-if="type !== 'SchneideZahn' && type !== 'EckZahn'" class="unten-mitte" :style='styleUntenMitteMitte' >
+        <spezial-form-round />
+      </div>
+      <div v-if="type === 'SchneideZahn' || type === 'EckZahn'" class="box unten-links"/>
+      <div v-if="type === 'SchneideZahn' || type === 'EckZahn'" class="unten-mitte">
+        <div class="box"/>
+        <div class="box"/>
+        <div class="box"/>
+      </div>
+      <div v-if="type === 'SchneideZahn' || type === 'EckZahn'" class="box unten-rechts"/>
+    </div>
   </div>
 </template>
 
 <script>
-import SchneideZahn from './ZahnTyp/SchneideZahn.vue'
-import EckZahn from './ZahnTyp/EckZahn.vue'
-import VormahlZahn from './ZahnTyp/VormahlZahn.vue'
-import MahlZahn from './ZahnTyp/MahlZahn.vue'
-import WeisheitsZahn from './ZahnTyp/WeisheitsZahn.vue'
+//import SchneideZahn from './ZahnTyp/SchneideZahn.vue'
+//import EckZahn from './ZahnTyp/EckZahn.vue'
+//import VormahlZahn from './ZahnTyp/VormahlZahn.vue'
+//import MahlZahn from './ZahnTyp/MahlZahn.vue'
+//import WeisheitsZahn from './ZahnTyp/WeisheitsZahn.vue'
+
+import WurzelSelector from './ZahnTyp/Spezial/WurzelSelector.vue'
+import SpezialFormRound from './ZahnTyp/Spezial/SpezialFormRound0.vue'
+
 export default {
   name: "Zahnecard",
   components: {
-    SchneideZahn,
-    EckZahn,
-    VormahlZahn,
-    MahlZahn,
-    WeisheitsZahn
+    //SchneideZahn,
+    //EckZahn,
+    //VormahlZahn,
+    //MahlZahn,
+    //WeisheitsZahn,
+    WurzelSelector,
+    SpezialFormRound
   },
   props:{
     zahnStyle: [],
@@ -38,6 +56,11 @@ export default {
       default: false
     },
   }
+  //<eck-zahn v-if="type === 'EckZahn'" :zahn-style="zahnStyle" :wurzeln="wurzeln" :isUnterKiefer="isUnterKiefer" />
+  //<vormahl-zahn v-if="type === 'VormahlZahn'" :zahn-style="zahnStyle" :wurzeln="wurzeln" :isUnterKiefer="isUnterKiefer" />
+  //<mahl-zahn v-if="type === 'MahlZahn'" :zahn-style="zahnStyle" :wurzeln="wurzeln" :isUnterKiefer="isUnterKiefer" />
+  //<weisheits-zahn v-if="type === 'WeisheitsZahn'" :zahn-style="zahnStyle" :wurzeln="wurzeln" :isUnterKiefer="isUnterKiefer" />
+
 
 }
 </script>
@@ -47,5 +70,44 @@ export default {
   width: 80px;
   height: 80px;
   background-color: aquamarine;
+}
+.zzz{
+  width: 100%;
+  height: 100%;
+}
+.ZahnCardAufbau{
+  padding-right: 10px;
+  padding-left: 10px;
+  padding-bottom: 3px;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 30% 40% 30%;
+  grid-template-rows: 35% 20% 45%;
+}
+.wurzel-selector{
+  grid-column-end: span 3;
+}
+.box{
+  background-color: white;
+  border-style: solid;
+  border-width: 2px;
+  border-radius: 4px;
+}
+.mitte-mitte{
+  border-radius: 4px;
+  margin: 3px 1px 1px;
+  z-index: 2;
+}
+.unten-mitte{
+  display: grid;
+  grid-template-rows: 33% 34% 33%;
+}
+.unten-mitte >*{
+  border-radius: 4px;
+}
+.unten-links{
+}
+.unten-rechts{
 }
 </style>
--- UI/dentistui/src/components/body/components/Tagesbehandlungen/Behandlungsraum/Zaehne/ZahnTyp/SchneideZahn.vue
+++ UI/dentistui/src/components/body/components/Tagesbehandlungen/Behandlungsraum/Zaehne/ZahnTyp/SchneideZahn.vue
@@ -1,36 +1,10 @@
 <template>
   <div class="all">
-    <div v-if="!isUnterKiefer" class="SchneideZahn" :style="styleSchneideZahn" >
-      <wurzel-selector class="wurzel-selector" :wurzeln="wurzeln"/>
-      <div />
-      <div class="box mitte-mitte"/>
-      <div />
-      <div class="box unten-links"/>
-      <div class="unten-mitte">
-        <div class="box"/>
-        <div class="box"/>
-        <div class="box"/>
-      </div>
-      <div class="box unten-rechts"/>
-    </div>
-    <div v-else class="SchneideZahn" :style="styleSchneideZahn" >
-      <div class="box unten-links"/>
-      <div class="unten-mitte">
-        <div class="box"/>
-        <div class="box"/>
-        <div class="box"/>
-      </div>
-      <div class="box unten-rechts"/>
-      <div />
-      <div class="box mitte-mitte"/>
-      <div />
-      <wurzel-selector class="wurzel-selector pHoch" :wurzeln="wurzeln" isUnterKiefer />
-    </div>
+    <slot></slot>
   </div>
 </template>
 
 <script>
-import WurzelSelector from './Spezial/WurzelSelector.vue'
 export default {
   name: "SchneideZahn",
   props:{
@@ -44,7 +18,6 @@ export default {
     },
   },
   components: {
-    WurzelSelector
   },
   computed:{
     styleSchneideZahn(){
@@ -63,50 +36,7 @@ export default {
 
 <style scoped>
 .all{
-  height: 100%;
-}
-.SchneideZahn{
-  padding-right: 10px;
-  padding-left: 10px;
   width: 100%;
   height: 100%;
-  display: grid;
-  grid-template-columns: 30% 40% 30%;
-}
-.wurzel-selector{
-  grid-column-end: span 3;
-}
-.SchneideZahn >*{
-}
-.mitte-mitte{
-  border-radius: 6px;
-  margin: 3px 1px 1px;
-  z-index: 2;
-}
-.unten-mitte{
-  border-radius: 6px;
-  display: grid;
-  grid-template-rows: 33% 34% 33%;
-}
-.unten-mitte >*{
-  border-radius: 6px;
-}
-.unten-links{
-  border-radius: 6px;
-  margin-bottom: 2px;
-}
-.unten-rechts{
-  border-radius: 4px;
-  margin-bottom: 2px;
-}
-.box{
-  background-color: white;
-  border-style: solid;
-  border-width: 2px;
-  border-radius: 4px;
-}
-.pHoch{
-  margin-top: -14px;
-  margin-bottom: 10px;
 }
 </style>
